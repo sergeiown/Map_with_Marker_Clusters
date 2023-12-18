bd46aa1e263488c91ac189ee2e7e55cc7019660c
Serhii Myshko <sergeiown@gmail.com>
2023-12-18 10:17:49 +0200

    update meta tags

--- js/styles.js
+++ js/styles.js
@@ -6,11 +6,13 @@ export function addStyles() {
             margin: 0;
             padding: 0;
             height: 100%;
+            width: 100vw;
             overflow: hidden;
         }
 
         #map {
             height: 100%;
+            width: 100vw;
             overflow: hidden;
         }`;
 
