3abd75b3c53ec9fd62cd9f597b7870e196005d68
Serhii Myshko <sergeiown@gmail.com>
2024-04-18 13:18:16 +0300

    update front line button

--- js/button.js
+++ js/button.js
@@ -7,57 +7,21 @@ export function createButton() {
     document.body.appendChild(buttonContainer);
 
     const openPageButton = document.createElement('button');
-    openPageButton.style.cursor = 'pointer';
-    openPageButton.style.border = '2px solid rgba(0, 0, 0, 0.2)';
-    openPageButton.style.borderRadius = '5px';
-    openPageButton.style.backgroundColor = 'rgba(255, 255, 255, 0.6)';
-    openPageButton.style.fontWeight = 'bold';
-    openPageButton.style.fontSize = '15px';
-    openPageButton.style.lineHeight = '1.2';
-    openPageButton.style.color = 'black';
-    openPageButton.style.webkitTextFillColor = 'black';
+    openPageButton.classList.add('leaflet-bar', 'leaflet-control', 'custom-button');
+    openPageButton.style.padding = '0';
+    openPageButton.style.width = '62px';
+    openPageButton.style.height = '62px';
 
-    openPageButton.addEventListener('mouseenter', () => {
-        openPageButton.style.backgroundColor = 'rgba(255, 255, 255, 0.7)';
-        openPageButton.style.position = 'relative';
-        openPageButton.style.zIndex = '1';
+    openPageButton.setAttribute('title', 'Front line map');
 
-        const overlay = document.createElement('div');
-        overlay.style.position = 'absolute';
-        overlay.style.top = '0';
-        overlay.style.left = '0';
-        overlay.style.width = '100%';
-        overlay.style.height = '100%';
-        overlay.style.backgroundColor = 'rgba(200, 200, 200, 0.4)';
-        overlay.style.borderRadius = '5px';
-        overlay.style.zIndex = '-1';
-        openPageButton.appendChild(overlay);
-    });
-
-    openPageButton.addEventListener('mouseleave', () => {
-        openPageButton.style.backgroundColor = 'rgba(255, 255, 255, 0.6)';
-        openPageButton.style.position = 'static';
-        openPageButton.style.zIndex = 'auto';
-
-        const overlay = openPageButton.querySelector('div');
-        if (overlay) {
-            openPageButton.removeChild(overlay);
-        }
-    });
-
-    openPageButton.addEventListener('mousedown', () => {
-        openPageButton.style.backgroundColor = 'rgba(255, 255, 255, 0.9)';
-    });
-
-    openPageButton.addEventListener('mouseup', () => {
-        openPageButton.style.backgroundColor = 'rgba(255, 255, 255, 0.6)';
-    });
+    const image = document.createElement('img');
+    image.src = './markers/front.png';
+    image.classList.add('center-image');
+    openPageButton.appendChild(image);
 
     openPageButton.addEventListener('click', () => {
         window.open('https://thepage.ua/ua/karta-liniyi-frontu-v-ukrayini', '_blank');
     });
-
-    openPageButton.innerHTML = 'Ознайомитися з поточною ситуацією<br>по тимчасово окупованих територіях';
 
     buttonContainer.appendChild(openPageButton);
 }
