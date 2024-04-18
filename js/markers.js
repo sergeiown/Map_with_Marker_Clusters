b3637c17281d8ade3770a066397374ee3b55cbec
Serhii Myshko <sergeiown@gmail.com>
2024-04-18 17:27:46 +0300

    add automatic closing of the popup

--- js/markers.js
+++ js/markers.js
@@ -12,16 +12,50 @@ export function addMarkers(map) {
                 });
 
                 const marker = L.marker([address.lat, address.lng], { icon: customIcon });
-                marker.bindPopup(`<b>${address.company}</b><br>${address.address}<br><small>${address.info}`);
+                const popupContent = `<b>${address.company}</b><br>${address.address}<br><small>${address.info}`;
+                const popupOptions = {
+                    closeButton: true,
+                    closeOnEscapeKey: true,
+                    closeOnClick: true,
+                    autoPan: true,
+                    keepInView: true,
+                };
+
+                marker.bindPopup(popupContent, popupOptions);
+
+                let popup;
 
                 marker.on('mouseover', function (e) {
+                    popup = this.getPopup();
                     this.openPopup();
+                    map.on('mousemove', onMouseMove);
                 });
+
+                marker.on('popupclose', function (e) {
+                    map.off('mousemove', onMouseMove);
+                });
+
+                function onMouseMove(e) {
+                    if (popup && isCursorOutsidePopup(e)) {
+                        this.closePopup();
+                    }
+                }
+
+                function isCursorOutsidePopup(e) {
+                    const popupPosition = popup._container.getBoundingClientRect();
+                    const cursorPosition = { x: e.originalEvent.clientX, y: e.originalEvent.clientY };
+                    return (
+                        cursorPosition.x < popupPosition.left ||
+                        cursorPosition.x > popupPosition.right ||
+                        cursorPosition.y < popupPosition.top ||
+                        cursorPosition.y > popupPosition.bottom
+                    );
+                }
 
                 markers.addLayer(marker);
             });
 
             map.addLayer(markers);
         })
-        .catch((error) => console.error('Error loading addresses:', error));
+        .catch((error) => console.error('Помилка при завантаженні адрес:', error));
 }
