3f12396c697459a327eec8b6d1bef88841a0cf4a
Serhii Myshko <sergeiown@gmail.com>
2024-04-24 11:28:06 +0300

    add additional layers for a better experience

--- js/layers.js
+++ js/layers.js
@@ -9,6 +9,18 @@ export const osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/
     ext: 'png',
 });
 
+export const stadiaAlidadeSmooth = L.tileLayer(
+    'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}',
+    {
+        attribution:
+            '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        opacity: 1,
+        minZoom: 5,
+        maxZoom: 17,
+        ext: 'png',
+    }
+);
+
 export const topoLayer = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.{ext}', {
     attribution: '© OpenTopoMap contributors',
     opacity: 1,
@@ -16,6 +28,30 @@ export const topoLayer = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{
     maxZoom: 17,
     ext: 'png',
 });
+
+export const stadiaStamenTerrain = L.tileLayer(
+    'https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.{ext}',
+    {
+        attribution:
+            '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        opacity: 1,
+        minZoom: 5,
+        maxZoom: 17,
+        ext: 'png',
+    }
+);
+
+export const stadiaAlidadeSatellite = L.tileLayer(
+    'https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}',
+    {
+        attribution:
+            '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        opacity: 1,
+        minZoom: 5,
+        maxZoom: 19,
+        ext: 'jpg',
+    }
+);
 
 export const esriNatGeoWorldMap = L.tileLayer(
     'https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}',
@@ -28,7 +64,10 @@ export const esriNatGeoWorldMap = L.tileLayer(
 );
 
 export const baseLayers = {
-    Адміністративна: osmLayer,
-    Топографічна: topoLayer,
+    'Адміністративна 1': osmLayer,
+    'Адміністративна 2': stadiaAlidadeSmooth,
+    'Топографічна 1': topoLayer,
+    'Топографічна 2': stadiaStamenTerrain,
+    'Супутникова мапа': stadiaAlidadeSatellite,
     'National Geographic': esriNatGeoWorldMap,
 };
